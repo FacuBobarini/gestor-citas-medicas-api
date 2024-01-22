1a2beb00869a2d3523d6c3f9728b8e4380eec0ad
FacuBobarini <facubobarini@gmail.com>
2024-01-21 23:37:45 -0300

    add dni, starttime,endtime and workschedule field and change .pre(save) add functionality to create schedule to workschedule field

--- src/schema/doctor.schema.ts
+++ src/schema/doctor.schema.ts
@@ -2,6 +2,7 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Schema as MongooseSchema, ObjectId, Types, UpdateQuery } from "mongoose";
 import { Specializations } from "./specialization.schema";
 import { HttpException, HttpStatus } from "@nestjs/common";
+import { format, addMinutes  } from "date-fns";
 
 
 @Schema({
@@ -10,47 +11,79 @@ import { HttpException, HttpStatus } from "@nestjs/common";
 export class Doctors {
     @Prop({
         required: true,
-        trim: true
     })
     name: string;
 
     @Prop({
         required: true,
-        trim: true
     })
     last_name: string;
 
     @Prop({
         unique: true,
         required: true,
-        trim: true
+    })
+    dni: string;
+
+    @Prop({
+        unique: true,
+        required: true,
     })
     email: string;
 
     @Prop({
         required: true,
-        trim: true
-    })
-    password: string;
-
+      })
+    startTime: string;
     
+    @Prop({
+        required: true,
+    })
+    endTime: string;
+
+    @Prop({
+        type: [String], 
+        default: [] 
+    })
+    workSchedule: string;
 
     @Prop({
         required: true,
-        trim: true,
+    })
+    password: string; 
+
+    @Prop({
+        required: true,
         type: MongooseSchema.Types.ObjectId,
         ref: 'Specializations',        
     })
-    specialization: Specializations  
-
-  
+    specialization: Specializations
 };
 
  const DoctorSchema:MongooseSchema =  SchemaFactory.createForClass(Doctors)
 
  DoctorSchema.pre("save",async function(next){
-    if(await this.model(Specializations.name).findById(this.specialization)){ 
-    next()}
+
+    const specializationInfo:any = await this.model(Specializations.name).findById(this.specialization)
+    if(specializationInfo){ 
+    
+        const doctorSchedule = () => {
+
+            const schedule: string[] = [];
+
+            let actualAppointment = new Date(`2000-01-01T${this.startTime}:00`);
+            while (actualAppointment < new Date(`2000-01-01T${this.endTime}:00`)) {
+                schedule.push(format(actualAppointment, 'HH:mm'));
+                actualAppointment = addMinutes(actualAppointment, specializationInfo.appointmentTime);
+             }
+
+             return schedule;
+          }
+          
+        this.workSchedule = doctorSchedule()
+
+        next()
+    }
 
     const err:HttpException = new HttpException('Specialization Not Found', HttpStatus.NOT_FOUND)
 
@@ -59,12 +92,13 @@ export class Doctors {
  });
 
  DoctorSchema.pre("findOneAndUpdate",async function(next){
+
     const update: UpdateQuery<ObjectId> = this.getUpdate()
+    if(!update.specialization){
+        next()
+    }
     
-    if(!update.specialization){next()}
-    
-    const idUpdate: Types.ObjectId = new Types.ObjectId(update.specialization)
-    
+    const idUpdate: Types.ObjectId = new Types.ObjectId(update.specialization)  
     if(await this.model.db.collection('specializations').findOne({ _id:  idUpdate })){
         next()
     }
