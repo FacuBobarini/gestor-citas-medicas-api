651e4dffb2d46d7b421a2fa851d3dac1f0fdd609
FacuBobarini <facubobarini@gmail.com>
2024-02-13 16:41:48 -0300

    add bcrypt encrypt to password in addNewPatient function and limit output fields on findPatientById and findAllPatients functions

--- src/doctor/doctor.service.ts
+++ src/doctor/doctor.service.ts
@@ -3,22 +3,32 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Doctor } from 'src/doctor/interface/doctor.interface';
 import { Doctors } from 'src/schema/doctor.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class DoctorService {
     constructor (@InjectModel(Doctors.name) private doctorModel: Model<Doctors>){}
 
     findAllDoctors(){
-        return  this.doctorModel.find().populate('specialization','name description').exec()
+        return  this.doctorModel.find()
+        .populate('specialization','-_id name description')
+        .select('-_id -createdAt -updatedAt -__v' ).exec()
     }
 
     findDoctorById(id: string){
-        return  this.doctorModel.findById(id).populate('specialization','name description').exec()
+        return  this.doctorModel.findById(id)
+        .populate('specialization','-_id name description')
+        .select('-_id -createdAt -updatedAt -__v' ).exec()
     }
 
     addNewDoctor(doctor: any){
-        const newDoctor= doctor
 
+        const {password}= doctor
+
+        const hash:string = bcrypt.hashSync(password, (bcrypt.genSaltSync(10)));
+
+        doctor.password = hash
+        
         return new this.doctorModel(doctor).save()
     }
 
